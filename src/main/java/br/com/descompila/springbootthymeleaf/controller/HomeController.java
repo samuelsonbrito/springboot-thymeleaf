44c250dabd3f91f0c3b53c0c3970ac3049940612
Samuelson <samuelsonma@gmail.com>
2024-01-24 20:11:39 -0400

    primeiro commit

---
+++ src/main/java/br/com/descompila/springbootthymeleaf/controller/HomeController.java
@@ -0,0 +1,14 @@
+package br.com.descompila.springbootthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/home")
+    public String getHomePage() {
+        return "home_page";
+    }
+}
